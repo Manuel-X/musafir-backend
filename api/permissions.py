b2ac63665f5a9f43159da72a53561c121f150281
samirahisham <engineersamiraawad@gmail.com>
2019-10-15 10:59:57 +0300

    removed not needed stuff

--- api/permissions.py
+++ api/permissions.py
@@ -3,11 +3,11 @@ from rest_framework.permissions import BasePermission
 
 
 #for modifying the booking (later) -samira  [ICEBOX FEATURE]
-class IsBookingOwner(BasePermission):
-	message = "You must be the owner of this booking"
+# class IsBookingOwner(BasePermission):
+# 	message = "You must be the owner of this booking"
 
-	def has_object_permission(self, request, view, obj):
-		if request.user.is_staff or (obj.user == request.user):
-			return True
-		else:
-			return False
+# 	def has_object_permission(self, request, view, obj):
+# 		if request.user.is_staff or (obj.user == request.user):
+# 			return True
+# 		else:
+# 			return False
